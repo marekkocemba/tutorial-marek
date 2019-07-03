ba21fb6220a82f23ce6ff04427a63604c647d8b7
Marek Kocemba <mkocemb@unity.pl>
2019-07-03 09:44:44 +0200

    Inicjalny szkielet aplikacji

---
+++ src/main/java/pl/unity/tutorial/marek/TutorialMarekApplication.java
@@ -0,0 +1,13 @@
+package pl.unity.tutorial.marek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialMarekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialMarekApplication.class, args);
+	}
+
+}
